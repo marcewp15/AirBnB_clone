64947422cad0a4c9878f6cbf37d412f01141d800
arcewp15 <970@holbertonschool.com>
2020-02-14 12:05:51 -0500

    [Update] adding documentation - pep8 style

--- models/base_model.py
+++ models/base_model.py
@@ -1,37 +1,33 @@
 #!/usr/bin/python3
 """
-
+AirBnb project - BaseModel class
 """
+
+
 import uuid
 from datetime import datetime
 
+
 class BaseModel:
-    """
-
-    """
+    """ This class defines all common attributes/methods for other classes """
     def __init__(self):
-        """
-
-        """
+        """ method constructor """
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.update_at = datetime.now()
 
     def __str__(self):
-        """
-
-        """
-        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
+        """ print a readable string """
+        return "[{}] ({}) {}".format(
+            self.__class__.__name__, self.id, self.__dict__)
 
     def save(self):
-        """
-
-        """
+        """ updates with the current datetime """
         self.update_at = datetime.now()
 
     def to_dict(self):
         """
-
+        returns a dictionary with all keys/value of __dict__ of the instance
         """
         dictnew = {}
         dictnew['__class__'] = self.__class__.__name__
