100adc8bcc2da9bf6c6e842b3e949f1d7451232d
Juan-Bogota <1213@holbertonschool.com>
2020-02-16 10:04:02 -0500

    [Fix] task number 5 file Storage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -3,9 +3,10 @@
 
 """
 
-
+import os
 import json
 from models.base_model import BaseModel
+import models
 
 class FileStorage:
     """
@@ -28,30 +29,41 @@ class FileStorage:
         new_2 = obj.id
         new = new_1 + '.' + new_2
         self.__objects[new] = obj
-
+        return self.__objects
 
     def save(self):
         """
         serialization
         """
-        my_dict = {}
-        with open(self.__file_path, 'a') as f:
+        dict_add = {}
+        with open(self.__file_path, 'w') as f:
             for key, value in self.__objects.items():
-                my_dict[key] = value.to_dict()
-            f.write(json.dumps(my_dict))
+                dict_add[key] = value.to_dict()
+            json.dump(dict_add, f)
             #f.write(m)
 
     def reload(self):
         """
         deserialization
         """
+
         classes = {'BaseModel':BaseModel}
         try:
+            with open(self.__file_path, 'r', encoding='utf-8') as myFile:
+                file_store = json.load(myFile)
+                for key, value in file_store.items():
+                    if '__class__' in value:
+                        self.__objects[key] = classes[value['__class__']](**value)
+        except:
+            pass
+        """
+        try:
             with open(self.__file_path, 'r') as f1:
-                file_store = json.loads(f1.read())
-            for key, value in file_store.items():
-                val = models.classes[value['__class__']](**value)
+                file_store = json.load(f1)
+                for key, value in file_store.items():
+                    val = models.classes[value['__class__']](**value)
                 #val = models.classes[file_store[key]['__class__']](**file_store[key])
                 self.__objects[key] = val
         except:
             pass
+        """
