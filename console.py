9e119668e12947203a172194088b4f79ffd0525f
Juan-Bogota <1213@holbertonschool.com>
2020-02-18 11:22:19 -0500

    [update] :key: Add file Console

--- console.py
+++ console.py
@@ -46,6 +46,55 @@ class HBNBCommand(cmd.Cmd):
         print("** no instance found **")
         pass
 
+    def do_all(self, args):
+        """
+
+        """
+        classes = ['BaseModel']
+        if len(args) == 0:
+            all_objs = storage.all()
+            for obj_id in all_objs.keys():
+                obj = all_objs[obj_id]
+                print(obj)
+        elif args not in classes:
+            print("** class doesn't exist **")
+        elif 'BaseModel' in classes:
+            all_objs = storage.all()
+            for obj_id in all_objs.keys():
+                obj = all_objs[obj_id]
+                print(obj)
+
+
+    def do_destroy(self, args):
+        """
+
+        """
+        classes = ['BaseModel']
+        words = args.split(' ')
+        if len(args) == 0:
+            print("** class name missing **")
+            return
+        elif words[0] not in classes:
+            print("** class doesn't exist **")
+            return
+        elif len(words) == 1:
+            print("** instance id missing **")
+            return
+        all_objs = storage.all()
+        s = 'BaseModel' + '.' + words[1]
+        for key , value in all_objs.items():
+            if s in key:
+                del all_objs[str(s)]
+                storage.save()
+                return
+        print("** no instance found **")
+
+    def do_update(self):
+        """
+
+        """
+        print("I am UPDATE")
+
     def emptyline(self):
         """ Method called when an empty line is entered in response
         to the prompt."""
