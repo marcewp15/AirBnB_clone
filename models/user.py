ce22050f86123ca766b20f9fe3126b4e38cac409
Juan-Bogota <1213@holbertonschool.com>
2020-02-18 18:44:29 -0500

    [Update] Fix pep8 and Documentation

--- models/user.py
+++ models/user.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
-""" """
+"""User class that inherits from BaseModel"""
 
 from models.base_model import BaseModel
 
 
 class User(BaseModel):
-    """ class attributes """
+    """ class attributes of User"""
     email = ""
     password = ""
     first_name = ""
