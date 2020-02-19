0162d20b9512e47b2f970b35605fee5a7b4024a8
Juan-Bogota <1213@holbertonschool.com>
2020-02-19 15:40:56 +0000

    [Update] correction files Unittest

---
+++ tests/test_models/test_engine/test_file_storage.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+Unit tests for Base class
+"""
+
+
+import unittest
+from models import storage
+from models.base_models import BaseModel
+import pep8
+
+
+class Test_Base(unittest.TestCase):
+    """Base class tests"""
+    def test_validate(self):
+        """ validate the id number, without args """
+        pass
+
+    def test_pep8_conformance(self):
+        """Test that we conform to PEP8."""
+        pep8style = pep8.StyleGuide(quiet=True)
+        result = pep8style.check_files(['models/base.py',
+                                        'models/rectangle.py',
+                                        'models/square.py'])
+        self.assertEqual(result.total_errors, 0,
+                         "Found code style errors (and warnings).")
